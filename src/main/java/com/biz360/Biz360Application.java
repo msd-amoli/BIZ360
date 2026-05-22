ae8f4c727541494c286e157746bf2ebe64ddceb2
Admin <madhuamoli16@gmail.com>
2026-05-22 17:07:25 +0400

    setup base project structure

---
+++ src/main/java/com/biz360/Biz360Application.java
@@ -0,0 +1,13 @@
+package com.biz360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Biz360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Biz360Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/biz360/Biz360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Biz360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
