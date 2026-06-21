d21ec699d52849d25e19eabcc48a3a47a86135af
Admin <madhuamoli16@gmail.com>
2026-06-21 15:51:16 +0400

    Restart ai service and fixed files

--- ai-service/services/prediction_service.py
+++
@@ -1,9 +0,0 @@
-import joblib
-
-# Load model once when server starts
-model = joblib.load("model/intent_model.pkl")
-
-
-def predict_intent(message: str):
-    prediction = model.predict([message])
-    return prediction[0]
